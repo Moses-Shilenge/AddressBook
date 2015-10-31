6ab5696549b927629d2247a23c9817aced1ffc14
Moses Shilenge <ushilmo@gmail.com>
2015-10-31 04:14:07 +0000

    Initial commit with n-tier architecture

---
+++ AddressBook.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contacts.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
